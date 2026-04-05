7089fb12d032039d31e8c0471cf28adfce02861d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-05 23:29:14 +0400

    feat(header): create a logo with text

--- christmas-shop/src/components/header/header.rs
+++ christmas-shop/src/components/header/header.rs
@@ -5,7 +5,10 @@ pub fn Header() -> impl IntoView {
     view! {
         <header class="header">
             <div class="container">
-                <img src="/assets/icons/snowflake.svg" alt="logo" />
+                <div class="logo">
+                    <img src="/assets/icons/snowflake.svg" alt="logo" />
+                    <p class="text">the gifts</p>
+                </div>
                 <div class="burger-container">
                     <div class="burger-menu"></div>
                 </div>
